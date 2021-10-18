3a9fa0a786158f1b18b2670863927e672634fe7d
FritzPere <fritznobbe@gmail.com>
2021-10-18 16:45:21 +0200

    adding

--- accuracy_all_subjects.py
+++ accuracy_all_subjects.py
@@ -7,7 +7,7 @@ Created on Mon Jul 26 14:52:31 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 subjects=list(range(25,36)) 
 bands=[-1,0,1,2] 
@@ -20,17 +20,22 @@ names=[]
 for band in band_dic.values():
     names.append(band+'1')
     names.append(band+'2')
-
+ind = np.arange(22)
 for space in spaces:
     fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(20  , 30))
     plt.rcParams['xtick.labelsize']=18
+    plt.rcParams['ytick.labelsize']=24
     subj=25
     for i in range(6):
         for j in range(2):
             if j==1 and i==5:
                 break
             perf=np.zeros((4,2,10))
-            k=0
+            k=0            
+            test_size_subj=pd.read_csv('results/intensities/subject_'+str(subj)+'/'+space+'/subject_table.csv')['test size']
+            test_size_1_mean=test_size_subj[0:-1:2].mean()
+            test_size_2_mean=test_size_subj[1:8:2].mean()
+            
             for b in bands:
                 band=band_dic[b]
                 temp1=np.load('results/intensities/subject_'+str(subj)+'/'+space+'/'+band+'/session'+str(1)+'/topological_clf/'+band+'perf_intensity.npy')[0,1]
@@ -63,9 +68,11 @@ for space in spaces:
             axes[i][j].axis(xmin=-0.8,xmax=2.4,ymin=0,ymax=1.05)
             axes[i][j].set_ylabel('accuracy',fontsize=20)
             axes[i][j].set_title('subject '+str(subj),fontsize=24)
+            axes[-1][-1].bar(ind[(subj-25)*2],test_size_1_mean,color='b')
+            axes[-1][-1].bar(ind[(subj-25)*2+1],test_size_2_mean,color='r')
             subj=subj+1
 
-            plt.setp(axes,xticks=[-0.6,-0.2,0.2, 0.6,1,1.4,1.8,2.2],xticklabels=names)
+            plt.setp(axes,xticks=[-0.6,-0.2,0.2, 0.6,1,1.4,1.8,2.2],yticks=[0,0.5,1],xticklabels=names)
     
     fig.suptitle('Accuracies for different subjects and frequancy bands\n for topological classifier (Silhouettes)',fontsize=36)
     fig.tight_layout(pad=0.5)
@@ -76,6 +83,7 @@ for space in spaces:
     #Also for 1nn
     fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(20  , 30))
     plt.rcParams['xtick.labelsize']=18
+    plt.rcParams['ytick.labelsize']=24
     subj=25
     for i in range(6):
         for j in range(2):
